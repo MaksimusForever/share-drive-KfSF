cdd709878c74c9fe35f82d0833cd0b190c42d1f8
Maksym <133545912+MaksimusForever@users.noreply.github.com>
2026-02-01 12:34:19 +0300

    Delete client directory

--- client/src/components/Badge.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Badge({ children, type = 'popular' }) {
-  return (
-    <span className={`badge badge-${type}`}>
-      {children}
-    </span>
-  );
-}
